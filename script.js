db5067c388b1e1bce6b61d88b4a5d1fcfa75d391
adrian831 <82091302+adrian831@users.noreply.github.com>
2024-03-01 13:01:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ function visualizzaJSON() {
     .then(response => response.json())
     .then(data => {
       var jsonContainer = document.getElementById('jsonContainer');
-      jsonContainer.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+      var formattedJSON = JSON.stringify(data, null, 2);
+      var article = document.createElement('article');
+      article.innerHTML = `<pre>${formattedJSON}</pre>`;
+      jsonContainer.appendChild(article);
     })
     .catch(error => console.error('Si è verificato un errore durante il recupero dei dati JSON:', error));
 }
