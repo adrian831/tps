cb4193f186bc2e55f826ff78b0e36561f9d665ba
adrian831 <82091302+adrian831@users.noreply.github.com>
2024-03-01 12:53:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ function visualizzaJSON() {
     .then(response => response.json())
     .then(data => {
       var jsonContainer = document.getElementById('jsonContainer');
-      jsonContainer.textContent = JSON.stringify(data, null, 2);
+      data.forEach(racchetta => {
+        var racchettaDiv = document.createElement('div');
+        racchettaDiv.classList.add('racchetta-dettaglio');
+        racchettaDiv.innerHTML = `
+          <h2>${racchetta.marca} ${racchetta.modello}</h2>
+          <p><strong>Peso:</strong> ${racchetta.peso}</p>
+          <p><strong>Lunghezza:</strong> ${racchetta.lunghezza}</p>
+          <p><strong>Area della testa:</strong> ${racchetta.area_testa}</p>
+          <p><strong>Schema corde:</strong> ${racchetta.schema_corde}</p>
+          <p><strong>Prezzo:</strong> ${racchetta.prezzo}</p>
+          <p><strong>Disponibilità colori:</strong> ${racchetta.disponibilità_colori.join(', ')}</p>
+        `;
+        jsonContainer.appendChild(racchettaDiv);
+      });
     })
     .catch(error => console.error('Si è verificato un errore durante il recupero dei dati JSON:', error));
 }
