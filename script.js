96db2a1009f4495cbfebe45ba7dd1acf91993ac9
adrian831 <82091302+adrian831@users.noreply.github.com>
2024-03-01 13:06:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,32 @@ function visualizzaJSON() {
     .then(data => {
       var jsonContainer = document.getElementById('jsonContainer');
       var formattedJSON = JSON.stringify(data, null, 2);
-      var article = document.createElement('article');
-      article.innerHTML = `<pre>${formattedJSON}</pre>`;
-      jsonContainer.appendChild(article);
+      jsonContainer.innerHTML = `<article>${formatAsArticle(formattedJSON)}</article>`;
     })
     .catch(error => console.error('Si è verificato un errore durante il recupero dei dati JSON:', error));
 }
+
+function formatAsArticle(jsonString) {
+  var json = JSON.parse(jsonString);
+  var articleContent = '';
+  for (var key in json) {
+    if (json.hasOwnProperty(key)) {
+      articleContent += `<h2>${capitalizeFirstLetter(key)}</h2>`;
+      if (typeof json[key] === 'object') {
+        articleContent += formatObject(json[key]);
+      } else {
+        articleContent += `<p>${json[key]}</p>`;
+      }
+    }
+  }
+  return articleContent;
+}
+
+function formatObject(obj) {
+  var content = '<ul>';
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      content += `<li><strong>${capitalizeFirstLetter(key)}:</strong> ${obj[key]}</li>`;
+    }
+  }
+ 
