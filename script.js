b7e05fb41dba5deb0cc4a54926bfe44547e4f686
adrian831 <82091302+adrian831@users.noreply.github.com>
2024-03-01 13:12:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,33 +7,24 @@ function visualizzaJSON() {
     .then(response => response.json())
     .then(data => {
       var jsonContainer = document.getElementById('jsonContainer');
-      var formattedJSON = JSON.stringify(data, null, 2);
-      jsonContainer.innerHTML = `<article>${formatAsArticle(formattedJSON)}</article>`;
+      jsonContainer.innerHTML = formatAsArticle(JSON.stringify(data));
     })
     .catch(error => console.error('Si è verificato un errore durante il recupero dei dati JSON:', error));
 }
 
 function formatAsArticle(jsonString) {
-  var json = JSON.parse(jsonString);
+  var data = JSON.parse(jsonString);
   var articleContent = '';
-  for (var key in json) {
-    if (json.hasOwnProperty(key)) {
-      articleContent += `<h2>${capitalizeFirstLetter(key)}</h2>`;
-      if (typeof json[key] === 'object') {
-        articleContent += formatObject(json[key]);
-      } else {
-        articleContent += `<p>${json[key]}</p>`;
-      }
-    }
-  }
+  data.forEach(racchetta => {
+    articleContent += `<article>
+                          <h2>${racchetta.marca} ${racchetta.modello}</h2>
+                          <p><strong>Peso:</strong> ${racchetta.peso}</p>
+                          <p><strong>Lunghezza:</strong> ${racchetta.lunghezza}</p>
+                          <p><strong>Area della testa:</strong> ${racchetta.area_testa}</p>
+                          <p><strong>Schema corde:</strong> ${racchetta.schema_corde}</p>
+                          <p><strong>Prezzo:</strong> ${racchetta.prezzo}</p>
+                          <p><strong>Disponibilità colori:</strong> ${racchetta.disponibilità_colori.join(', ')}</p>
+                        </article>`;
+  });
   return articleContent;
 }
-
-function formatObject(obj) {
-  var content = '<ul>';
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      content += `<li><strong>${capitalizeFirstLetter(key)}:</strong> ${obj[key]}</li>`;
-    }
-  }
- 
